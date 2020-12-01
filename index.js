35785279142c7503a807d8545c076fdc3809cc0a
Max Edmands <443070+maxedmands@users.noreply.github.com>
2020-11-30 21:28:48 -0800

    Formatting

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const fs = require('fs');
+const fs = require("fs");
 
 main().catch((error) => console.log(error.stack));
 
 async function main() {
-  const data = await parseFile('input.txt');
+  const data = await parseFile("input.txt");
+
   for (let i = 0; i < data.length; i++) {
     for (let j = i; j < data.length; j++) {
       for (let k = j; k < data.length; k++) {
-        const inputI = data[i];
-        const inputJ = data[j];
-        const inputK = data[k];
-        if (inputI + inputJ + inputK == 2020) {
-          console.log(inputI, inputJ, inputK);
-          console.log(inputI * inputJ * inputK);
+        const iVal = data[i];
+        const jVal = data[j];
+        const kVal = data[k];
+
+        if (iVal + jVal + kVal == 2020) {
+          console.log(iVal, jVal, kVal);
+          console.log(iVal * jVal * kVal);
         }
       }
     }
@@ -20,6 +22,6 @@ async function main() {
 }
 
 async function parseFile(filename) {
-  const result = fs.readFileSync(filename, {encoding: 'utf-8'});
+  const result = fs.readFileSync(filename, { encoding: "utf-8" });
   return result.split("\n").map((v) => parseInt(v));
 }

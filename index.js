d813be51888df164be43247d2b221e85bff5fab5
Max Edmands <443070+maxedmands@users.noreply.github.com>
2020-11-30 22:04:42 -0800

    Adds stats

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const fs = require("fs");
 
-main().catch((error) => console.log(error.stack));
-
 async function main() {
-  console.log("----");
   const data = await parseFile("input.txt");
 
   for (let i = 0; i < data.length; i++) {
@@ -12,6 +9,7 @@ async function main() {
     for (let j = i; j < data.length; j++) {
       const jVal = data[j];
       const runningSum = iVal + jVal;
+      if (runningSum > 2020) continue;
 
       for (let k = j; k < data.length; k++) {
         const kVal = data[k];
@@ -26,6 +24,16 @@ async function main() {
 }
 
 async function parseFile(filename) {
+  process.stdout.write(`Reading from ${filename}. `);
   const result = fs.readFileSync(filename, { encoding: "utf-8" });
-  return result.split("\n").map((v) => parseInt(v));
+  process.stdout.write(`${result.length} chars, `);
+  const lines = result.split("\n");
+  process.stdout.write(`${lines.length} lines\n`);
+  return lines.map((v) => parseInt(v));
 }
+
+const startTime = Date.now();
+console.log("----");
+main()
+  .catch((error) => console.log(error.stack))
+  .finally(() => console.log(`Done in ${Date.now() - startTime}ms`));

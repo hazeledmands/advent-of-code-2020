6e13ef91d4455c22c216f77791e6aba026d56160
Max Edmands <443070+maxedmands@users.noreply.github.com>
2020-12-06 22:10:53 -0800

    Day 7.1

--- index.js
+++ index.js
@@ -4,20 +4,76 @@ import { File, Grammar, Rule, Lexeme } from "@demands/text-parsing";
 
 async function main() {
   const grammar = new Grammar([
-    new Lexeme("person", { re: /[a-z]+/, evaluate: (p) => p.read().split("") }),
-    new Lexeme("separator", { re: /\n/, ignore: true }),
-    new Rule("GroupList", {
-      syntax: [["Group", "separator", "separator", "GroupList"], ["Group"]],
+    new Lexeme("contain", { re: /contain/, ignore: true }),
+    new Lexeme("bags", { re: /bags?/, ignore: true }),
+
+    new Lexeme("quantity", { re: /[0-9]+/, evaluate: (q) => parseInt(q) }),
+    new Lexeme("noOtherBags", { re: /no other bags/, ignore: true }),
+    new Lexeme("newLine", { re: /\n/, ignore: true }),
+    new Lexeme("period", { re: /\./, ignore: true }),
+    new Lexeme("comma", { re: /,/, ignore: true }),
+    new Lexeme("space", { re: / /, ignore: true }),
+    new Lexeme("description", { re: /[a-z]+ [a-z]+/ }),
+
+    new Rule("RuleList", {
+      syntax: [["Rule", "newLine", "RuleList"], ["Rule"]],
     }),
-    new Rule("Group", {
-      syntax: [["person", "separator", "Group"], ["person"]],
+    new Rule("Rule", {
+      syntax: [["Bag", "space", "contain", "space", "BagList", "period"]],
+    }),
+    new Rule("BagList", {
+      syntax: [
+        ["BagQuantity", "comma", "space", "BagList"],
+        ["BagQuantity"],
+        ["noOtherBags"],
+      ],
+      evaluate: (l) => {
+        const contents = new Map();
+        for (let q of l.parts) {
+          let [quantity, bag] = q.value();
+          if (contents.has(bag))
+            contents.set(bag, contents.get(bag) + quantity);
+          else contents.set(bag, quantity);
+        }
+        return contents;
+      },
+    }),
+    new Rule("BagQuantity", {
+      syntax: [["quantity", "space", "Bag"]],
+    }),
+    new Rule("Bag", {
+      syntax: [["description", "space", "bags"]],
+      evaluate: (b) => b.parts[0].read(),
     }),
   ]);
 
   const file = await File.loadFrom("./input.txt");
-  const ast = grammar.parse(file, "GroupList");
+  const ast = grammar.parse(file, "RuleList");
 
-  console.log(ast.value());
+  const rules = ast.value();
+  console.log(rules);
+
+  const invertedList = new Map();
+  for (let [bag, contents] of rules) {
+    for (let [key] of contents.entries()) {
+      if (!invertedList.has(key)) invertedList.set(key, new Set());
+      invertedList.get(key).add(bag);
+    }
+  }
+
+  const remainingNodes = ["shiny gold"];
+  const found = new Set();
+  while (remainingNodes.length > 0) {
+    const curr = remainingNodes.pop();
+
+    if (!invertedList.has(curr)) continue;
+    const toAdd = invertedList.get(curr);
+    for (const node of toAdd) {
+      remainingNodes.push(node);
+      found.add(node);
+    }
+  }
+  console.log(found);
 }
 
 function log() {
